b057d757fd863a8007833cdbfd189848f0ecf285
passivegoose <boris.mikhail09200@gmail.com>
2024-07-25 16:42:42 +0300

    small fixes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,17 +11,12 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
 
-        User first = new User("Ivan", "Ivanov", (byte) 19);
-        User second = new User("Boris", "Borisov", (byte) 20);
-        User third = new User("Karabas", "Barabas", (byte) 54);
-        User fourth = new User("Van", "Darkholme", (byte) 51);
-
         userService.createUsersTable();
 
-        userService.saveUser(first.getName(), first.getLastName(), first.getAge());
-        userService.saveUser(second.getName(), second.getLastName(), second.getAge());
-        userService.saveUser(third.getName(), third.getLastName(), third.getAge());
-        userService.saveUser(fourth.getName(), fourth.getLastName(), fourth.getAge());
+        userService.saveUser("Ivan", "Ivanov", (byte) 19);
+        userService.saveUser("Boris", "Borisov", (byte) 20);
+        userService.saveUser("Karabas", "Barabas", (byte) 54);
+        userService.saveUser("Van", "Darkholme", (byte) 51);
 
         List<User> users = userService.getAllUsers();
         users.forEach(System.out::println);
